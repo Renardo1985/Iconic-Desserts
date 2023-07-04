0aaa00dd1e155670f60c0b6c339e0d584221c4a1
Renardo Williams <rennwill@gmail.com>
2023-07-03 21:24:48 -0400

    Initial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function Home() {
-//   const [image, setImage] = useState("");
-
-//   useEffect(() => {
-//     fetch("http://localhost:3001/games")
-//       .then((r) => r.json())
-//       .then((data) => {
-//         setImage(data[Math.floor(Math.random() * data.length)].thumbnail);
-//       })
-//       .catch((e) => alert(e.message + " data please start json server"));
-//   }, []);
-
-//   if (!image) return <h2>Loading...</h2>;
-
   return (
     <div className="home">
       <div className="image-container">
